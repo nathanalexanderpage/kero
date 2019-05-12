527260a5987e3ae0f9f3031a0d35f2e5b846b28c
mtom92 <torresmoranm@gmail.com>
2019-05-12 10:45:19 -0700

    added basic structure

---
+++ client-side/src/reducer/ProfileReducer.js
@@ -0,0 +1,4 @@
+export default (state= {} , action) => {
+  let newState = {...state}
+
+}
--- client-side/src/reducer/RootReducer.js
+++ client-side/src/reducer/RootReducer.js
@@ -1,6 +1,14 @@
 import {combineReducers} from 'redux';
 import board from './BoardReducer';
+import profile from './ProfileReducer';
+import task from './TaskReducer';
+import project from './ProjectReducer';
+import sprint from './SprintReducer';
 
 export default combineReducers({
-  board
+  board,
+  profile,
+  task,
+  project,
+  sprint
 })
