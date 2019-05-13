c87bab1bae203c5aa7c4e22935cfc5b454fef570
shelmbreck <samantha.helmbreck@gmail.com>
2019-05-13 15:20:57 -0700

    added in sprint modal to project.js and task modal into sprint.js

--- client-side/src/pages/AdminProfile.js
+++ client-side/src/pages/AdminProfile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button,  Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from 'reactstrap';
 import '../App.css';
-import * as AdminProfileActions from '../actions/AdminProfileActions'
 
 class AdminProfile extends Component {
 
@@ -25,61 +24,61 @@ class AdminProfile extends Component {
     if(this.props.user){
       return (
 
-             <Container >
-               <Row>
-                 <Col>
-                   <img  id="userprofile" src={this.props.user.image}  />
-                 </Col>
-                 <Col>
-                   <h2>Hello again, {this.props.user.name} You are an admin!</h2>
-                   <h4>Your email is : {this.props.user.email}</h4>
-                   <h4>Your role is : {this.props.user.role}</h4>
-                   <h4>Your are working in :  {this.props.user.project}</h4>
-                   </Col>
-	               </Row>
-	               <Row>
-	                 <Col>
-	                   <Form inline onSubmit={(e) => e.preventDefault()}>
-	                       <Button color="danger" onClick={this.toggle}>New Project</Button>
-	                   </Form>
-	                   <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} >
-	                       <ModalHeader toggleProjectCard={this.toggle.projectCard}>Create a New Project</ModalHeader>
-	                       <ModalBody>
-	                         <Label>Title</Label>
-	                           <Input
-	                             type="text"
-	                             name="title"
-	                             placeholder="date placeholder"
-	                           />
-	                        <Label>Start Date</Label>
-	                          <Input
-	                            type="date"
-	                            name="startdate"
-	                            placeholder="date placeholder"
-	                          />
-	                        <Label>End Date</Label>
-	                          <Input
-	                            type="date"
-	                            name="finishdate"
-	                            placeholder="date placeholder"
-	                          />
-	                        <Label>Purpose</Label>
-	                          <Input
-	                            type="textarea"
-	                            name="purpose"
-	                            placeholder="Write something"
-	                            rows={5}
-	                             />
-	                           <Label > Author </Label>
-	                          <Input
-	                            plaintext value={this.props.user.name}
-	                            />
-	                       </ModalBody>
-	                       <ModalFooter>
-	                           <Button color="primary" onClick={this.toggle}>Create</Button>{' '}
-	                          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-	                      </ModalFooter>
-	                	</Modal>
+            <Container >
+              <Row>
+                <Col>
+                  <img  id="userprofile" src={this.props.user.image}  />
+                </Col>
+                <Col>
+                  <h2>Hello again, {this.props.user.name} You are an admin!</h2>
+                  <h4>Your email is : {this.props.user.email}</h4>
+                  <h4>Your role is : {this.props.user.role}</h4>
+                  <h4>Your are working in :  {this.props.user.project}</h4>
+                </Col>
+							</Row>
+							<Row>
+								<Col>
+									<Form inline onSubmit={(e) => e.preventDefault()}>
+										<Button color="danger" onClick={this.toggle}>New Project</Button>
+									</Form>
+									<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} >
+									<ModalHeader toggleProjectCard={this.toggle.projectCard}>Create a New Project</ModalHeader>
+									<ModalBody>
+										<Label>Title</Label>
+										<Input
+											type="text"
+											name="title"
+											placeholder="date placeholder"
+												/>
+										<Label>Start Date</Label>
+										<Input
+											type="date"
+											name="startdate"
+											placeholder="date placeholder"
+												/>
+										<Label>End Date</Label>
+										<Input
+											type="date"
+											name="finishdate"
+											placeholder="date placeholder"
+												/>
+										<Label>Purpose</Label>
+										<Input
+											type="textarea"
+											name="purpose"
+											placeholder="Write something"
+											rows={5}
+												/>
+										<Label > Author </Label>
+										<Input
+											plaintext value={this.props.user.name}
+												/>
+									</ModalBody>
+									<ModalFooter>
+										<Button color="primary" onClick={this.toggle}>Create</Button>{' '}
+										<Button color="secondary" onClick={this.toggle}>Cancel</Button>
+									</ModalFooter>
+								</Modal>
                 </Col>
               </Row>
             </Container>
