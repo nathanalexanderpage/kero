36c48cc1cfbc43923392934c472d9f6f61d311f3
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-13 13:07:41 -0700

    undo of revert

--- client-side/src/pages/AdminProfile.js
+++ client-side/src/pages/AdminProfile.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button,  Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from 'reactstrap';
 import '../App.css';
+import * as AdminProfileActions from '../actions/AdminProfileActions'
 
 class AdminProfile extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.changeUnmountOnClose = this.changeUnmountOnClose.bind(this);
+  }
+
+  toggle() {
+      this.setState(prevState => ({
+          modal: !prevState.modal
+      }));
+  }
+
+  changeUnmountOnClose(e) {
+      let value = e.target.value;
+      this.setState({ unmountOnClose: JSON.parse(value) });
+  }
+
   render() {
     if(this.props.user){
       return (
@@ -17,6 +40,51 @@ class AdminProfile extends Component {
                    <h4>Your email is : {this.props.user.email}</h4>
                    <h4>Your role is : {this.props.user.role}</h4>
                    <h4>Your are working in :  {this.props.user.project}</h4>
+                   </Col>
+	               </Row>
+	               <Row>
+	                 <Col>
+	                   <Form inline onSubmit={(e) => e.preventDefault()}>
+	                       <Button color="danger" onClick={this.toggle}>New Project</Button>
+	                   </Form>
+	                   <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} >
+	                       <ModalHeader toggle={this.toggle}>Create a New Project</ModalHeader>
+	                       <ModalBody>
+	                         <Label>Title</Label>
+	                           <Input
+	                             type="text"
+	                             name="title"
+	                             placeholder="date placeholder"
+	                           />
+	                        <Label>Start Date</Label>
+	                          <Input
+	                            type="date"
+	                            name="startdate"
+	                            placeholder="date placeholder"
+	                          />
+	                        <Label>End Date</Label>
+	                          <Input
+	                            type="date"
+	                            name="finishdate"
+	                            placeholder="date placeholder"
+	                          />
+	                        <Label>Purpose</Label>
+	                          <Input
+	                            type="textarea"
+	                            name="purpose"
+	                            placeholder="Write something"
+	                            rows={5}
+	                             />
+	                           <Label > Author </Label>
+	                          <Input
+	                            plaintext value={this.props.user.name}
+	                            />
+	                       </ModalBody>
+	                       <ModalFooter>
+	                           <Button color="primary" onClick={this.toggle}>Create</Button>{' '}
+	                           <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+	                       </ModalFooter>
+	                   </Modal>
                  </Col>
                </Row>
              </Container>
@@ -30,5 +98,11 @@ class AdminProfile extends Component {
       );
   }
 }
+
+const mapStateToProps = (state) => {
+	  return {
+	    modal: state.modal
+	  }
+	}
 
 export default AdminProfile;
