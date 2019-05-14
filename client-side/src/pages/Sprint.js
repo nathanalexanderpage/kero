8e1cd2aa34beeb84b2c3232011d0ecf1ca9de0d4
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-14 09:51:48 -0700

    removed double render issue

--- client-side/src/pages/Sprint.js
+++ client-side/src/pages/Sprint.js
@@ -15,10 +15,6 @@ class Sprint extends Component {
     this.changeUnmountOnClose = this.changeUnmountOnClose.bind(this);
   }
 
-
-  render() {
-
-
   toggle() {
       this.setState(prevState => ({
           modal: !prevState.modal
@@ -39,62 +35,62 @@ class Sprint extends Component {
     }
     return(
       <Container >
-              <Row>
-                <Col>
-                  <img  id="userprofile" src={this.props.user.image}  />
-                </Col>
-                <Col>
-                  <h2>Hello again, {this.props.user.name} You are an admin!</h2>
-                  <h4>Your email is : {this.props.user.email}</h4>
-                  <h4>Your role is : {this.props.user.role}</h4>
-                  <h4>Your are working in :  {this.props.user.sprint}</h4>
-                  </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Form inline onSubmit={(e) => e.preventDefault()}>
-                    <Button color="danger" onClick={this.toggle}>New Task</Button>
-                  </Form>
-                  <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} >
-                  <ModalHeader toggle={this.toggle}>Create a New Task</ModalHeader>
-                  <ModalBody>
-                    <Label>Title</Label>
+        <Row>
+          <Col>
+            <img  id="userprofile" src={this.props.user.image}  />
+          </Col>
+          <Col>
+            <h2>Hello again, {this.props.user.name} You are an admin!</h2>
+            <h4>Your email is : {this.props.user.email}</h4>
+            <h4>Your role is : {this.props.user.role}</h4>
+            <h4>Your are working in :  {this.props.user.sprint}</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form inline onSubmit={(e) => e.preventDefault()}>
+              <Button color="danger" onClick={this.toggle}>New Task</Button>
+            </Form>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} >
+            <ModalHeader toggle={this.toggle}>Create a New Task</ModalHeader>
+            <ModalBody>
+              <Label>Title</Label>
+              <Input
+                type="text"
+                name="title"
+                placeholder="date placeholder"
+                  />
+                  <Label>Start Date</Label>
+                  <Input
+                    type="date"
+                    name="startdate"
+                    placeholder="date placeholder"
+                      />
+                  <Label>End Date</Label>
                     <Input
-                      type="text"
-                      name="title"
-                      placeholder="date placeholder"
+                    type="date"
+                    name="finishdate"
+                    placeholder="date placeholder"
+                      />
+                    <Label>Purpose</Label>
+                    <Input
+                      type="textarea"
+                      name="purpose"
+                      placeholder="Write something"
+                      rows={5}
                         />
-                        <Label>Start Date</Label>
-                        <Input
-                          type="date"
-                          name="startdate"
-                          placeholder="date placeholder"
-                            />
-                        <Label>End Date</Label>
-                          <Input
-                          type="date"
-                          name="finishdate"
-                          placeholder="date placeholder"
-                            />
-                          <Label>Purpose</Label>
-                          <Input
-                            type="textarea"
-                            name="purpose"
-                            placeholder="Write something"
-                            rows={5}
-                              />
-                            <Label > Author </Label>
-                            <Input
-                              plaintext value={this.props.user.name}/>
-                          </ModalBody>
-                        <ModalFooter>
-                          <Button color="primary" onClick={this.toggle}>Create</Button>{' '}
-                          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                    </ModalFooter>
-                  </Modal>
-                </Col>
-              </Row>
-            </Container>
+                      <Label > Author </Label>
+                      <Input
+                        plaintext value={this.props.user.name}/>
+                    </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>Create</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+              </ModalFooter>
+            </Modal>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 };
