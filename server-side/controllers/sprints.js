0bd42e4b2f0b46a7b23389749dac041ba6737dfb
mtom92 <torresmoranm@gmail.com>
2019-05-14 18:19:04 -0700

    added spring logic to server side

--- server-side/controllers/sprints.js
+++ server-side/controllers/sprints.js
@@ -1,12 +1,7 @@
 //Required models
-<<<<<<< HEAD
 require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
-=======
-let express = require('express')
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
 
 //router instances
 let router = express.Router()
@@ -14,7 +9,6 @@ let router = express.Router()
 //include models
 let db = require('../models')
 
-<<<<<<< HEAD
 //get tasks
 router.post('/get', (req, res) => {
   console.log("PROJECTS HIT");
@@ -26,42 +20,22 @@ router.post('/get', (req, res) => {
   })
   .catch( err => {
     console.log('error in get /tasks', err);
-=======
-//GET sprints
-router.get('/', (req, res) => {
-  db.Sprint.find()
-  .then(foundSprint => {
-    res.send(foundSprint)
-  })
-  .catch( err => {
-    console.log('error in get /sprints', err);
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
     res.status(500).send('Something went wrong. Contact administrator')
   })
 })
 
 //post tasks
-<<<<<<< HEAD
 router.post('/post', (req, res) => {
   console.log('In the POST /project/ route');
   console.log('ESTE ES EL USUARIO', req.user);
   console.log(req.body);
-  req.body.project = ""
+  req.body.project =
   db.Sprint.create(req.body)
   .then(createdProject => {
     res.send(createdProject)
   })
   .catch( err => {
     console.log('error in post /Projects', err);
-=======
-router.post('/', (req, res) => {
-  db.Sprint.create(req.body)
-  .then(createdSprint => {
-    res.send(createdSprint)
-  })
-  .catch( err => {
-    console.log('error in post /sprints', err);
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
     res.status(500).send('Something went wrong. Contact administrator')
   })
 
@@ -69,21 +43,12 @@ router.post('/', (req, res) => {
 
 //get tasks/:id
 router.get('/:id', (req, res) => {
-<<<<<<< HEAD
   db.Project.findById(req.params.id)
   .then(foundProject => {
     res.send(foundProject)
   })
   .catch( err => {
     console.log('error in get /Project/:id', err);
-=======
-  db.Sprint.findById(req.params.id)
-  .then(foundSprint => {
-    res.send(foundSprint)
-  })
-  .catch( err => {
-    console.log('error in get sprints/:id', err);
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
     res.status(500).send('Something went wrong. Contact administrator')
   })
 })
@@ -91,7 +56,6 @@ router.get('/:id', (req, res) => {
 //put tasks
 router.put('/:id', (req, res) => {
   //args : {where}, data , {options}
-<<<<<<< HEAD
   db.Project.findOneAndUpdate(
     { _id: req.params.id},
     req.body ,
@@ -101,44 +65,21 @@ router.put('/:id', (req, res) => {
   })
   .catch( err => {
     console.log('error in put /Project/:id', err);
-=======
-  db.Sprint.findOneAndUpdate(
-    { _id: req.params.id},
-    req.body ,
-    {new: true, useFindAndModify:false }) //this will return what was updated
-  .then(editedSprint => {
-    res.send(editedSprint)
-  })
-  .catch( err => {
-    console.log('error in put sprints/:id', err);
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
     res.status(500).send('Something went wrong. Contact administrator')
   })
 })
 
 //delete tasks
 router.delete('/:id', (req, res) => {
-<<<<<<< HEAD
   db.Project.findOneAndDelete({
-=======
-  db.Sprint.findOneAndDelete({
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
     _id: req.params.id
   },{ useFindAndModify: false})
   .then(() => {
     res.status(204).send()
   })
   .catch( err => {
-<<<<<<< HEAD
     console.log('error in delete /Projects/:id', err);
     res.status(500).send('Something went wrong. Contact administrator')
   })
 })
 module.exports = router
-=======
-    console.log('error in delete /sprints/:id', err);
-    res.status(500).send('Something went wrong. Contact administrator')
-  })
-})
-module.exports = router
->>>>>>> 051f7ca0529444b6e168d5501b638b01e96c1b0c
