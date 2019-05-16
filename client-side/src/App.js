13f4442d98b494b55f0de7614d27cb9a4cab584c
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-16 12:38:49 -0700

    minor

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -50,6 +50,8 @@ class App extends Component {
     function projectsList(projRet) {
       console.log(`POST ${SERVER_URL}/projects/get`);
       let token = localStorage.getItem('serverToken');
+      console.log(token)
+
       axios.post(`${SERVER_URL}/projects/get`, {}, {
         headers: {
           'Authorization' : `Bearer ${token}`
