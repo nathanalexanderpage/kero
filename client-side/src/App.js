c3b10cd7b139f0602c1681792430067dfc8794bf
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-18 15:31:40 -0700

    async componentDidMount config

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -54,6 +54,7 @@ class App extends Component {
 
   loadUserData = () => {
     console.log("INSIDE componentDidMount");
+
     function sprintList(sprintRet) {
       console.log(`GET ${SERVER_URL}/sprints/admin`);
       let token = localStorage.getItem('serverToken');
@@ -102,8 +103,13 @@ class App extends Component {
         sprints: dataLists[0],
         tasks: dataLists[1]
       });
-      console.log("all the user information", this.state.sprints);
-      console.log(this.state.user);
+      console.log("all the state information");
+      console.log(this.state);
+      if (this.state.user) {
+        console.log('USER IS ROLE:');
+        console.log(this.state.user.role);
+        
+      }
     });
   }
 
